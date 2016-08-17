73937a6fc449ffa200afe8a19fb4348f79089353
32xolf <32xolf@gmail.com>
2016-08-17 17:25:20 +0800

    Adding Theme

--- ChitChat/ChitChatClient/MainClient.cs
+++ ChitChat/ChitChatClient/MainClient.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ChitChatClient
 {
-    public partial class Form1 : Form
+    public partial class MainClient : Form
     {
-        public Form1()
+        public MainClient()
         {
             InitializeComponent();
         }
