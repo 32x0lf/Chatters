2e7ac2f8684727c0172218b8e7dbe780696f8508
32x0lf <32xolf@gmail.com>
2016-09-14 23:19:22 +0800

    b

--- Chatters/ChitChatClient/FrmLogin.cs
+++ Chatters/ChitChatClient/FrmLogin.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChitChatClient.Utils;
 
 namespace ChitChatClient
 {
     public partial class FrmLogin : Form
     {
+        Settings settings = new Settings();
         public FrmLogin()
         {
             InitializeComponent();
+            
+            txtserverip.Text = Properties.Settings.Default.ServerIP;
+            txtport.Text = Properties.Settings.Default.Port;
+
+            settings.ServerIp = txtserverip.Text;
+            settings.ServerPort = txtport.Text;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+        //    settings.ServerIp = txtserverip.Text;
+        //    settings.ServerPort = txtport.Text;
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.ServerIP = txtserverip.Text;
+            Properties.Settings.Default.Port = txtport.Text;
+            Properties.Settings.Default.Save();
+
+            settings.ServerIp = txtserverip.Text;
+            settings.ServerPort = txtport.Text;
+
         }
     }
 }
---
+++ Chatters/ChitChatClient/Utils/Client_Chat.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChitChatAPI;
+using ChitChatAPI.Enums;
+
+namespace ChitChatClient.Utils
+{
+    class Client_Chat : IConnect
+    {
+        bool Logged = false;
+        
+        public string clientName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ConnTypes conntype
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Server
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public string Typeuser
+        {
+            get
+            {
+                return "Client";
+            }
+        }
+
+        public void ChangePassword(string uname, string oldpassword, string newpassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnected(bool client)
+        {
+           return Logged;
+        }
+
+        public bool IsDisconnected(string client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRegistered(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsServerisdown(string server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsVisible(string uname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void statusMessage(string message, string uname)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
